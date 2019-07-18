cb1de23af186cb7ff28a4ae0d5da9710ea14cfd2
shandryll <shandryll94@gmail.com>
2019-07-18 14:11:38 -0300

    Starting codes

---
+++ Fundamentos/tipos_basicos.py
@@ -0,0 +1,24 @@
+# Boolean
+print(True)
+print(False)
+
+# Float
+print(1.2 + 2.3)
+
+# Int
+print(2 + 6)
+
+# String
+print('Hello World!')
+
+# String + Int
+print('Você é ' + 3 * 'muito ' + 'legal')
+print('Você é', 10, '!')
+
+# List
+print([1, 2, 3])
+
+# Dictionary
+print({'nome:': 'Andal',
+       'atk': 1900,
+       'def': 1000})
