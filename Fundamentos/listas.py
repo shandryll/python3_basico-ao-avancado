e5c72f55880a7a0dd0d07a17df3e4f08c98f0891
shandryll <shandryll94@gmail.com>
2019-07-22 14:43:31 -0300

    Insert codes

---
+++ Fundamentos/listas.py
@@ -0,0 +1,18 @@
+lista = []
+print(type(lista))
+print(len(lista))
+
+lista.append(1)
+lista.append(5)
+print(lista)
+print(len(lista))
+
+nova_lista = [1, 5, 'Ana', 'Bia']
+print(nova_lista)
+print(len(nova_lista))
+
+nova_lista.remove(5)
+print(nova_lista)
+
+nova_lista.reverse()
+print(nova_lista)
---
+++ Fundamentos/strings.py
@@ -0,0 +1,26 @@
+nome = 'Ana Paula'
+print(nome[0])
+print(nome[6])
+print(nome[-3])
+print(nome[4:])
+print(nome[-5:])
+print(nome[:3])
+print(nome[2:5])
+print(nome[::-1])
+
+numeros = '1234567890'
+print(numeros)
+print(numeros[::])
+print(numeros[::2])
+print(numeros[1::2])
+print(numeros[::-1])
+print(numeros[::-2])
+
+frase = 'Python é cabuloso'
+print('py' not in frase)
+print('cabuloso' in frase)
+print(len(frase))
+print(frase.lower())
+print('py' in frase)
+print(frase.split())
+print(frase.split('o'))
